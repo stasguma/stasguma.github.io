de5dd5f3a4bb9eecb1cae39359e6b74f733057c9
Stas Guma <mcguma@mail.ru>
2015-12-15 12:45:18 +0200

    Update script.js

--- GoIT/JavaScript/lesson1-2/script.js
+++ GoIT/JavaScript/lesson1-2/script.js
@@ -17,53 +17,53 @@ console.log( pow() );
 //////////////// ПЕРВЫЙ ВАРИАНТ //////////////
 /////////////////////////////////////////////
 // var names = [];
-//
-// for (var i = 0; i < 5; i++) {
-//   names[i] = prompt('Введите имя');
-// }
-//
-// console.log(names);
-//
-// var userName = prompt('Введите имя пользователя');
-// var flag = false;
-//
-// for (var i = 0; i < names.length; i++) {
-//   if (userName === names[i]) {
-//     flag = true;
-//     break;
-//   }
-// }
-//
-// if (flag) {
-//   alert(userName + ", вы успешно вошли");
-// } else {
-//   alert("Пользователя, " + userName + ", не существует");
-// }
+
+ for (var i = 0; i < 5; i++) {
+   names[i] = prompt('Введите имя');
+ }
+
+ console.log(names);
+
+ var userName = prompt('Введите имя пользователя');
+ var flag = false;
+
+ for (var i = 0; i < names.length; i++) {
+   if (userName === names[i]) {
+     flag = true;
+     break;
+   }
+ }
+
+ if (flag) {
+   alert(userName + ", вы успешно вошли");
+ } else {
+   alert("Пользователя, " + userName + ", не существует");
+ }
 
 ///////////////////////////////////////////////
 /////////////// ВТОРОЙ ВАРИАНТ ///////////////
 /////////////////////////////////////////////
-// var names = [];
-//
-// for (var i = 0; i < 5; i++) {
-//   names[i] = prompt('Введите имя');
-// }
-//
-// console.log(names);
-//
-// var userName = prompt('Введите имя пользователя');
-// var flag = false;
-//
-// for (var i = 0; i < names.length; i++) {
-//   if (names[i].indexOf( userName ) === 0) {
-//     flag = true;
-//     break;
-//   }
-// }
-//
-// if (flag) {
-//   alert(userName + ", вы успешно вошли");
-// } else {
-//
-//   alert("Пользователя, " + userName + ", не существует");
-// }
+ var names = [];
+
+ for (var i = 0; i < 5; i++) {
+   names[i] = prompt('Введите имя');
+ }
+
+ console.log(names);
+
+ var userName = prompt('Введите имя пользователя');
+ var flag = false;
+
+ for (var i = 0; i < names.length; i++) {
+   if (names[i].indexOf( userName ) === 0) {
+     flag = true;
+     break;
+   }
+ }
+
+ if (flag) {
+   alert(userName + ", вы успешно вошли");
+ } else {
+
+   alert("Пользователя, " + userName + ", не существует");
+ }
