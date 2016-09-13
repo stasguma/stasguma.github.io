38810b354e216de1fc7d2b42c5af8ee8f985e35a
Stas Guma <mcguma@mail.ru>
2016-09-13 22:43:52 +0300

    resume

---
+++ gh-page/gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    concatCSS = require('gulp-concat-css'),
+    cleanCSS = require('gulp-clean-css'),
+    concat = require('gulp-concat'),
+    uncss = require('gulp-uncss'),
+    uglify = require('gulp-uglify'),
+    pump = require('pump'),
+    imagemin = require('gulp-imagemin'),
+    spritesmith = require('gulp.spritesmith'),
+    watch = require('gulp-watch');
+
+gulp.task('css', function() {
+   return gulp.src('css/style.scss')
+   .pipe(sass().on('error', sass.logError))
+   // .pipe(concatCSS('main.css'))
+   // .pipe(uncss({
+   //      html: ['index.html']
+   //  }))
+   // .pipe( cleanCSS() )
+   .pipe(rename ('style.css') )
+   .pipe(gulp.dest('css/dist'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('js/src/*.js')
+    // .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(rename('main.min.js'))
+    .pipe(gulp.dest('js/dist'));
+});
+
+gulp.task('imagemin', function() {
+    gulp.src('img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('img/dist'));
+});
+
+gulp.task('watch', function () {
+        gulp.watch('css/style.scss', ['css']);
+});
+
+gulp.task('default', ['css', 'watch']);
