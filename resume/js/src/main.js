db667655fa5ecb2fadfd1db29a50aaf4a098cf94
Stas Guma <mcguma@mail.ru>
2016-09-21 22:07:29 +0300

    resume

---
+++ resume/js/src/main.js
@@ -0,0 +1,30 @@
+(function() {
+    new WOW().init(); // WOW.js
+
+    document.addEventListener('DOMContentLoaded', function () {
+
+        setTimeout(function () {
+            document.body.style.opacity = 1;
+        }, 1000);
+    });
+
+    var contacts = document.querySelector('.contacts');
+
+    contacts.addEventListener('mouseover', function (event) {
+        var target = event.target;
+        var icon = target.firstElementChild;
+
+        if (icon.tagName === 'I') {
+
+            icon.classList.add("animated", "bounceCustom");
+
+            contacts.addEventListener('mouseout', function (event) {
+
+                var icon = event.target.firstElementChild;
+                icon.classList.remove("animated", "bounceCustom");
+            });
+        }
+
+    });
+
+}());
