faa17d25b8e6a6b974ee9e0863b0d45a5713bbb3
Stas Guma <mcguma@mail.ru>
2017-02-12 12:35:58 +0200

    added block

--- resume/gulpfile.js
+++ resume/gulpfile.js
@@ -1,40 +1,68 @@
 var gulp = require('gulp'),
+	autoprefixer = require('gulp-autoprefixer'),
+	cleanCSS = require('gulp-clean-css'),
+	connect = require('gulp-connect'),
+	imagemin = require('gulp-imagemin'),
+	livereload = require('gulp-livereload'),
+	pump = require('pump'),
+	rename = require('gulp-rename'),
     sass = require('gulp-sass'),
-    rename = require('gulp-rename'),
-    concatCSS = require('gulp-concat-css'),
-    cleanCSS = require('gulp-clean-css'),
-    concat = require('gulp-concat'),
-    uncss = require('gulp-uncss'),
     uglify = require('gulp-uglify'),
-    pump = require('pump'),
-    imagemin = require('gulp-imagemin'),
-    spritesmith = require('gulp.spritesmith'),
-    watch = require('gulp-watch');
+	watch = require('gulp-watch');
 
-gulp.task('css', function() {
-   return gulp.src('css/src/style.scss')
-       .pipe(sass().on('error', sass.logError))
-       .pipe(gulp.dest('css/src'))
-       .pipe( cleanCSS() )
-       .pipe(rename ('main.min.css') )
-       .pipe(gulp.dest('css/dist'));
+
+gulp.task('html', function () {
+    return gulp.src(['index.html'])
+    .pipe(connect.reload());
 });
 
-gulp.task('scripts', function() {
-    return gulp.src('js/src/main.js')
-        .pipe(uglify())
-        .pipe(rename('main.min.js'))
-        .pipe(gulp.dest('js/dist'));
+gulp.task('css', function() {
+   return gulp.src(['scss/style.scss', 'scss/components/*'])
+   .pipe(sass().on('error', sass.logError))
+   .pipe(autoprefixer({
+            browsers: ['last 5 versions', 'IE >= 9']
+    }))
+   .pipe(rename ('main.css') )
+   .pipe(gulp.dest('dist/css'))
+   .pipe( cleanCSS() )
+   .pipe(rename ('main.min.css') )
+   .pipe(gulp.dest('dist/css'))
+   .pipe(connect.reload());
+});
+gulp.task('css2', function() {
+   return gulp.src(['scss/lib/animate.scss'])
+   .pipe(sass().on('error', sass.logError))
+   .pipe(autoprefixer({
+            browsers: ['last 5 versions', 'IE >= 9']
+    }))
+   .pipe( cleanCSS() )
+   .pipe(rename ('animate.min.css') )
+   .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('javascript', function() {
+  return gulp.src('js/main.js')
+    .pipe(uglify())
+    .pipe(rename('main.min.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(connect.reload());
 });
 
 gulp.task('imagemin', function() {
-    gulp.src('img/*')
+    gulp.src('img/*.*')
         .pipe(imagemin())
-        .pipe(gulp.dest('img/dist'));
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    // root: 'app',
+    livereload: true
+  });
 });
 
 gulp.task('watch', function () {
-    gulp.watch(['css/src/style.scss', 'js/src/main.js'], ['css', 'scripts']);
+        gulp.watch(['index.html', 'scss/style.scss', 'scss/components/*'], ['html', 'css']);
 });
 
-gulp.task('default', ['css', 'scripts', 'watch']);
+gulp.task('default', ['connect', 'html', 'css', 'watch']);
